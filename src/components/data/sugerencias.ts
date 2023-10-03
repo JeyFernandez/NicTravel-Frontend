bd78ed1b60064a66308e42c9857afc09a2bb134b
jasson <jeicobfer@gmail.com>
2023-10-02 20:12:16 -0600

    Realizando cambios en mi rama

---
+++ src/components/data/sugerencias.ts
@@ -0,0 +1,17 @@
+export const dataSugerencias = [
+  {
+    name: "Hoteles",
+    image:
+      "https://laverdadnoticias.com/__export/1585620552106/sites/laverdad/img/2020/03/30/clalasificacion_hoteles_2.jpg_1834093470.jpg",
+  },
+  {
+    name: "Restaurantes",
+    image:
+      "https://th.bing.com/th/id/R.0bbd273bab89c381a5dac5ba1baa0e05?rik=m4xXx5pSK4TQNQ&riu=http%3a%2f%2flideres.co%2fwp-content%2fuploads%2f2016%2f08%2frestaurante-2.jpg&ehk=uUgxvJ%2bIida7NcEwcqnqcjLOMByZJmZaj0Rhpdao2B0%3d&risl=&pid=ImgRaw&r=0",
+  },
+  {
+    name: "Lugares",
+    image:
+      "https://th.bing.com/th/id/R.d96ff91dac20ef266a028ed8687bdfff?rik=MkjXLPzqcZZNFA&riu=http%3a%2f%2fcos.h-cdn.co%2fassets%2f15%2f51%2f480x724%2f1450446691-gettyimages-500694179_master.jpg&ehk=TiLKKv4JG1CU91Lc15qtNvBkK%2fcSf8p9Ffa6pWwM62Y%3d&risl=&pid=ImgRaw&r=0",
+  },
+];
