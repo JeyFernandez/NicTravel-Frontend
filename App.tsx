1b8fd84c5d0e8e6d0cad9817b13d1d399baaeb92
Teodoro Obando <teo.obando09@gmail.com>
2023-10-23 22:55:07 -0600

    [ADD] LogBox para ignorar todos los logs en modo desarrollo

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import MainStack from './src/navigation/MainStack';
+
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
